276476c3013ea9ba39ea5f1c359f681aca02bbe4
Ke-Wei Ma <keweima@yahoo.com>
2016-07-28 00:32:58 -0400

    stuff

---
+++ plot_high_IBM.py
@@ -0,0 +1,15 @@
+"""Plot High prices for IBM"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("data/IBM.csv")
+    # TODO: Your code here
+	print df['High']
+	df['High'].plot()
+    plt.show()  # must be called to show plots
+
+
+if __name__ == "__main__":
+    test_run()
